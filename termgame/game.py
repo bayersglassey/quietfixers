f794fba783b18d4a1930598562f0473e58e8103e
Ben Ayers-Glassey <bayersglassey@gmail.com>
2025-08-31 12:19:57 -0700

    qfgame -> termgame

---
+++ termgame/game.py
@@ -0,0 +1,64 @@
+from typing import NamedTuple
+from time import monotonic, sleep
+
+from .screen import Screen
+from .keyboard import Keyboard
+
+
+class Event(NamedTuple):
+    # It's up to you to iterate over this, and call parse_key on its
+    # members if you want to extract key modifiers
+    keys: list[str]
+
+
+class Game:
+
+    def __init__(
+            self,
+            *,
+            screen: Screen = None,
+            keyboard: Keyboard = None,
+            fps: float = 15,
+            ):
+        self.screen = screen or Screen(64, 32)
+        self.keyboard = keyboard or Keyboard()
+        self.fps = fps
+        self.spf = 1 / fps
+
+    def get_text_input(self, prompt: str) -> str:
+        with self.keyboard.cooked():
+            text = input(prompt)
+        # WARNING: "clearing" the screen ends up meaning Gnome Terminal
+        # scrolls down past everything which had been drawn on it, which
+        # I think maybe we want to avoid... otherwise, after quitting the
+        # game, if you scroll up, you see every frame it ever rendered
+        # before terminal was cleared, if that makes sense.
+        #self.screen.clear()
+        return text
+
+    def run(self):
+        screen = self.screen
+        keyboard = self.keyboard
+
+        with screen.hide_cursor(), keyboard.raw():
+            screen.clear()
+            while True:
+                t0 = monotonic()
+
+                # Handle I/O
+                with keyboard.no_block():
+                    keys = keyboard.get_keys()
+                yield Event(keys=keys)
+
+                # Render
+                screen.reset_cursor()
+                screen.display()
+                screen.flush()
+                screen.clear_messages()
+
+                # Delay
+                t1 = monotonic()
+                took = t1 - t0
+                delay = self.spf - took
+                if delay > 0:
+                    sleep(delay)
--- termgame/paint.py
+++ termgame/paint.py
@@ -1,70 +1,9 @@
 import os
 
-from typing import NamedTuple
-from time import monotonic, sleep
-
 from .bitmap import Bitmap, get_colour_code
 from .screen import Screen
 from .keyboard import Keyboard, Key, KeyMod, parse_key
-
-
-class Event(NamedTuple):
-    # It's up to you to iterate over this, and call parse_key on its
-    # members if you want to extract key modifiers
-    keys: list[str]
-
-
-class Game:
-
-    def __init__(
-            self,
-            *,
-            screen: Screen = None,
-            keyboard: Keyboard = None,
-            fps: float = 15,
-            ):
-        self.screen = screen or Screen(64, 32)
-        self.keyboard = keyboard or Keyboard()
-        self.fps = fps
-        self.spf = 1 / fps
-
-    def get_text_input(self, prompt: str) -> str:
-        with self.keyboard.cooked():
-            text = input(prompt)
-        # WARNING: "clearing" the screen ends up meaning Gnome Terminal
-        # scrolls down past everything which had been drawn on it, which
-        # I think maybe we want to avoid... otherwise, after quitting the
-        # game, if you scroll up, you see every frame it ever rendered
-        # before terminal was cleared, if that makes sense.
-        #self.screen.clear()
-        return text
-
-    def run(self):
-        screen = self.screen
-        keyboard = self.keyboard
-
-        with screen.hide_cursor(), keyboard.raw():
-            screen.clear()
-            while True:
-                t0 = monotonic()
-
-                # Handle I/O
-                with keyboard.no_block():
-                    keys = keyboard.get_keys()
-                yield Event(keys=keys)
-
-                # Render
-                screen.reset_cursor()
-                screen.display()
-                screen.flush()
-                screen.clear_messages()
-
-                # Delay
-                t1 = monotonic()
-                took = t1 - t0
-                delay = self.spf - took
-                if delay > 0:
-                    sleep(delay)
+from .game import Game
 
 
 KEYS_TO_COLOURS = {
@@ -80,7 +19,7 @@ KEYS_TO_COLOURS = {
 }
 
 
-def drawtest(filename=os.path.join('scratch', 'drawtest.dat')):
+def main(filename=os.path.join('scratch', 'drawtest.dat')):
     """A simple "paint" program"""
     screen = Screen(64, 32)
     keyboard = Keyboard()
@@ -155,3 +94,7 @@ def drawtest(filename=os.path.join('scratch', 'drawtest.dat')):
         screen.print(''.join(to_ansi(c) for c in range(8)))
         screen.print(''.join(to_ansi(c + 8) for c in range(8)))
         tick += 1
+
+
+if __name__ == '__main__':
+    main()
